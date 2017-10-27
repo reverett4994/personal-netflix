68bdb54be24c90ae76e38a90b3de7f69a5dec0c9
Ryan Everett <reverett4994@gmail.com>
2017-10-27 04:45:06 -0400

    added validators for creating videos and other bug fixes

--- app/models/video.rb
+++ app/models/video.rb
@@ -4,6 +4,8 @@ class Video < ActiveRecord::Base
    belongs_to :series
    belongs_to :user
    validate :content_type
+   validate :series_check
+   validate :season_episode
    after_validation  :add_title_and_desc
    #validate :add_title_and_desc, :on => :create
    mount_uploader :content, ContentUploader
@@ -28,49 +30,75 @@ class Video < ActiveRecord::Base
     end
   end
 
-  def add_title_and_desc
-    @search = Tmdb::Search.new
-    if self.production_type.downcase == "tv"
-      @search.resource('tv')
-      if self.series
-        @search.query(self.series.title)
-        if @search.fetch.any?
-          @tv_series_id=@search.fetch[0]["id"]
-          @episode = Tmdb::Episode.detail(@search.fetch[0]["id"] , self.season, self.episode)
-          @episode_title= @episode["name"]
-          @episode_desc= @episode["overview"]
-          @episode_date= @episode["air_date"]
-          @pics = Tmdb::Episode.images(@tv_series_id, self.season, self.episode)
-          if @pics["stills"][1]==nil
-            @poster= "http://image.tmdb.org/t/p/w780/#{ @pics["stills"][0]["file_path"]}"
-          else
-            @poster= "http://image.tmdb.org/t/p/w780/#{ @pics["stills"][1]["file_path"]}"
-          end
-          self.date = @episode_date.to_date
-          self.poster = @poster
-          if self.title==""
-            self.title = @episode_title
-          end
-          if self.desc==""
-            self.desc = @episode_desc
-          end
-        end
+  def series_check
+    if self.production_type !=nil && self.production_type.downcase == "tv"
+      if self.series == nil
+        errors.add(:video, "must belong to a series")
       end
-    elsif self.production_type.downcase=="movie"
-      @search = Tmdb::Search.new
-      @search.resource('movie')
-      @search.query(self.title)
-      @movie_id=@search.fetch[0]["id"]
-      @movie = Tmdb::Movie.detail(@movie_id)
-      @movie_desc=@movie["overview"]
-      @movie_date=@movie["release_date"]
-      @poster= "http://image.tmdb.org/t/p/w780/#{ @movie["poster_path"]}"
-      @movie_img="http://image.tmdb.org/t/p/w780/#{ @movie["backdrop_path"]}"
-      self.poster=@poster
-      self.movie_img=@movie_img
-      self.desc=@movie_desc
-      self.date=@movie_date.to_date
     end
   end
+
+  def season_episode
+    if self.production_type !=nil && self.production_type.downcase == "tv"
+      if self.season == nil || self.episode == nil
+        errors.add(:video, "must have a season and episode number")
+      end
+    end
+  end
+
+  def add_title_and_desc
+    @search = Tmdb::Search.new
+    if self.production_type==nil
+      errors.add(:type, "must have a tv, movie or other type")
+    else
+      if self.production_type.downcase == "tv"
+        @search.resource('tv')
+        if self.series
+          @search.query(self.series.title)
+          if @search.fetch.any?
+            @tv_series_id=@search.fetch[0]["id"]
+            @episode = Tmdb::Episode.detail(@search.fetch[0]["id"] , self.season, self.episode)
+            @episode_title= @episode["name"]
+            @episode_desc= @episode["overview"]
+            @episode_date= @episode["air_date"]
+            if self.season != nil || self.episode != nil
+              @pics = Tmdb::Episode.images(@tv_series_id, self.season, self.episode)
+              if @pics["stills"][1]==nil
+                @poster= "http://image.tmdb.org/t/p/w780/#{ @pics["stills"][0]["file_path"]}"
+              else
+                @poster= "http://image.tmdb.org/t/p/w780/#{ @pics["stills"][1]["file_path"]}"
+              end
+
+              self.date = @episode_date.to_date
+              self.poster = @poster
+            end
+
+            if self.title==""
+              self.title = @episode_title
+            end
+            if self.desc=="" || self.desc==nil
+              self.desc = @episode_desc
+            end
+          end
+        end
+      elsif self.production_type.downcase=="movie"
+        @search = Tmdb::Search.new
+        @search.resource('movie')
+        @search.query(self.title)
+        @movie_id=@search.fetch[0]["id"]
+        @movie = Tmdb::Movie.detail(@movie_id)
+        @movie_desc=@movie["overview"]
+        @movie_date=@movie["release_date"]
+        @poster= "http://image.tmdb.org/t/p/w780/#{ @movie["poster_path"]}"
+        @movie_img="http://image.tmdb.org/t/p/w780/#{ @movie["backdrop_path"]}"
+        self.poster=@poster
+        self.movie_img=@movie_img
+        self.desc=@movie_desc
+        self.date=@movie_date.to_date
+      end
+    end
+  end
+
+
 
 end
