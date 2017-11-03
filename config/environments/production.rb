71010a0420f69c537e2bc5ae787fcb155fa9939f
Ryan Everett <reverett4994@gmail.com>
2017-11-03 03:51:56 -0400

    misc responsive design 5

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -76,4 +76,8 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+
+  #FOUND ON DEVISE PAGE TO USE WITH HEROKU
+  config.assets.initialize_on_precompile = false
 end
