5e9dc47b9c7ee627bfb820232be176959dfa009e
Ryan Everett <reverett4994@gmail.com>
2017-11-09 02:52:25 -0500

    added sendgrid

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -83,4 +83,20 @@ Rails.application.configure do
 
   config.assets.compile = true
   config.assets.digest = true
+
+
+
+  config.action_mailer.default_url_options = { host: 'CloudCinema.us'}
+config.mailer_sender = 'Cloud Cinema'
+
+config.action_mailer.delivery_method = :smtp
+ActionMailer::Base.smtp_settings = {
+  :user_name => ENV['SENDGRID_USERNAME'],
+  :password => ENV['SENDGRID_PASSWORD'],
+  :domain => 'heroku.com',
+  :address => 'smtp.sendgrid.net',
+  :port => 587,
+  :authentication => :plain,
+  :enable_starttls_auto => true
+}
 end
